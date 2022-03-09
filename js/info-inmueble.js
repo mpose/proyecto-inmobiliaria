d2a4ae920326f078f4596a1a0d381fd97c925722
fpelle24 <88352276+fpelle24@users.noreply.github.com>
2022-03-08 21:03:26 -0300

    Creación de la hoja de css y empezado a armado de el js para traer dato

--- js/info-inmueble.js
+++ js/info-inmueble.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", function(e){
 
+    /*div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active" data-bs-interval="1000">
+                                <img src="https://www.fondosanimales.com/800x600/sapo-hd.jpg" class="d-block w-100">
+                            </div>
+                            <div class="carousel-item" data-bs-interval="2000">
+                                <img src="https://www.todopaisajes.com/800x600/paisaje-de-flores.jpg" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="https://www.todopaisajes.com/800x600/planeta-espacial.jpg" class="d-block w-100" alt="...">
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval"
+                            data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval"
+                            data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>*/
 });
