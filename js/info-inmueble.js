053209744a68628309e5b8cb28523a359017a64c
fpelle24 <88352276+fpelle24@users.noreply.github.com>
2022-09-01 21:40:32 -0300

    Cambios en info inmueble
    mecha

--- js/info-inmueble.js
+++ js/info-inmueble.js
@@ -19,29 +19,39 @@ function infoInmueble(inmueble) {
     document.getElementById("infoInmueble").innerHTML = "";
     document.getElementById("localidadInmueble").innerHTML = "";
     document.getElementById("precioInmueble").innerHTML = "";
+    document.getElementById("iconitos").innerHTML = "";
     
     let titulo = "";
     let locInmueble = "";
     let ininfo = "";
     let precioInmueble = "";
+    let iconitos = "";
     
     titulo = `${inmueble.type} `+` en `+`${inmueble.category}`;
     locInmueble = `${inmueble.location}`+`,`+`${inmueble.departament}`
     precioInmueble = `${inmueble.currency} `+` ${inmueble.cost}` 
 
+    iconitos = `<div><h6 style="text-align:center;">Características</h6></div><div class="row"><div class="iconis col-3"><span class="material-icons-round">king_bed</span><p>${inmueble.bedrooms}</p></div>
+    <div class="iconis col-3"><span class="material-icons-round">bathtub</span><p> ${inmueble.toilets}</p></div>
+    <div class="iconis col-3"><span class="material-icons-round">home</span><p> ${inmueble.squareMetersBuilt}m²</p></div>
+    <div class="iconis col-3"><span class="material-icons-round">area_chart</span><p> ${inmueble.squareMeters}</p></div></div>`
+
+
+    document.getElementById("iconitos").innerHTML +=iconitos;
     document.getElementById("titulo").innerHTML += titulo;
     document.getElementById("localidadInmueble").innerHTML += locInmueble;
     document.getElementById("precioInmueble").innerHTML += precioInmueble;
+    
   
     
         ininfo =
             `
-            <div class="row">
-            
-                <div id="carouselExampleInterval" class="carousel slide col-xl-6" data-bs-ride="carousel"> 
+        <div class="row">
+            <div id="carousel" class="col-lg-6 col-md-12 sm-12">
+                <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel"> 
                     <div  class="carousel-inner" id="carrusell">
-                        <div id="coso">
-                         </div>
+                        <div id="coso"></div>
+                    </div>
                     
                 
                     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval"
@@ -55,18 +65,15 @@ function infoInmueble(inmueble) {
                             <span class="visually-hidden">Next</span>
                     </button>
                 </div>
-                </div>
-
-                    <div class="col-xl-6 col-md-6 xs-12" id="detallesPropiedad">
-                    <br>
-                        <div id="descriptionInmueble">
+            </div>
+                <br>
+                    <div class="col-lg-6 col-md-12 sm-12 py-4" id="descriptionInmueble">
                         <h4 id="descriptionTitulo" >Descripción</h4>
                                     <div id="lista"></div>
                                     <div id="lista2"></div>
                                 </br>
                                 </br>
-                            </div>
-                            <div>
+                    
                             <h4> Detalles </h4>
                             <ul class="list-group list-group-flush" id="listaDetalles">
                                 <li class="list-group-item"> Tipo de inmueble: ${inmueble.type} </li>
@@ -77,9 +84,12 @@ function infoInmueble(inmueble) {
                                 <li class="list-group-item"> Localidad: ${inmueble.location} </li>
                                 <li class="list-group-item"> Gastos comunes: ${inmueble.expenses}</li>
                                 <li class="list-group-item"> Garantías: ${inmueble.guarantee}</li>
-                            </ul>
-                            </div>
+                            </ul>   
+                        
                     </div>
+            </div>
+
+            <div class="row">
             </div>
             `
         document.getElementById("infoInmueble").innerHTML += ininfo;
@@ -106,7 +116,7 @@ function infoInmueble(inmueble) {
 
     for(let i = 0 ; i < inmueble.description.length; i++){
     
-        lista += `<p> ${inmueble.description[i]} </p>`
+        lista += `<p class="description"> ${inmueble.description[i]} </p>`
 
         document.getElementById("lista").innerHTML = lista
 
@@ -114,7 +124,7 @@ function infoInmueble(inmueble) {
 
     for(let i = 0 ; i < inmueble.AdditionalInformation.length; i++){
     
-        lista2 += `<p> ${inmueble.AdditionalInformation[i]} </p>`
+        lista2 += `<p class="description"> ${inmueble.AdditionalInformation[i]} </p>`
 
         document.getElementById("lista2").innerHTML = lista2
 
